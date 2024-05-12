e0473ab78e7fa41fb945b08de1f8900300dd90ea
lilit54548 <132910688+lilit54548@users.noreply.github.com>
2024-05-12 23:04:51 +0400

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -5,13 +5,7 @@ from typing import Optional
 from datetime import datetime 
 
 class ProjectInit(BaseModel):
-  """
-    Initialization model for creating a new project with basic details.
-    
-    Attributes:
-        project_description (str): A brief description of the project.
-        bandits_qty (conint(gt=1)): The quantity of bandits involved in the project, must be greater than 1.
-  """
+
     project_description: str
     bandits_qty: conint(gt=1)
 
@@ -20,15 +14,7 @@ class ProjectInit(BaseModel):
 
 
 class BanditsInit(BaseModel):
-   """
-    Initialization model for creating a new bandit associated with a project.
-    
-    Attributes:
-        bandit_id (conint(ge=1)): A unique identifier for the bandit, must be greater than or equal to 1.
-        project_id (conint(ge=1)): The identifier of the project this bandit is part of, must be greater than or equal to 1.
-        alpha (float, default=1.0): The alpha parameter for the bandit's beta distribution.
-        beta (float, default=1.0): The beta parameter for the bandit's beta distribution.
-    """
+
     bandit_id: conint(ge=1)
     project_id: conint(ge=1)
     alpha: float = 1.0
@@ -41,27 +27,12 @@ class BanditsInit(BaseModel):
 
     
 class UserEvent(BaseModel):
-   """
-      Model representing an event triggered by a user in relation to a bandit within a project.
-    
-      Attributes:
-        project_id (int): The identifier for the project this event is associated with.
-        bandit_id (int): The identifier of the bandit associated with this event.
-        event (int): An integer representing the type of event that occurred.
-    """
     project_id: int
     bandit_id: int
     event: int
     
 class UpdatePriors(BaseModel):
-   """
-    Model for updating the priors (alpha and beta) of a bandit based on new data.
-    
-    Attributes:
-        alpha (float): Updated alpha value reflecting prior or accumulated knowledge of success.
-        beta (float): Updated beta value reflecting prior or accumulated knowledge of failures.
-        n (int): The number of additional trials or interactions since the last update.
-   """
+
     alpha: float  
     beta: float  
     n: int 
